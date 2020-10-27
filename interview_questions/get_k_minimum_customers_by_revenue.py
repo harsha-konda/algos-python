af13af48a6da2826878f87eb382339677154479d
Harsha Konda <hkonda@andrew.cmu.edu>
2020-10-27 16:16:13 -0700

    Create get_k_minimum_customers_by_revenue.py

---
+++ interview_questions/get_k_minimum_customers_by_revenue.py
@@ -0,0 +1,7 @@
+"""
+insert(revenue): CustomerId  # auto incrementing customer Id
+insert(revenue , referrer) : CustomerId
+get_k_minimum_customers_by_revenue(k: Int, minRevenue: Int) : List<Customer>
+"""
+
+
