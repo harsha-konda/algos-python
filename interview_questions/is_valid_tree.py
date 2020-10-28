c20ebc97f9347cb3fe15530bbaddea300bbe66b2
Harsha Konda <hkonda@andrew.cmu.edu>
2020-10-28 14:12:57 -0700

    Create is_valid_tree.py

---
+++ interview_questions/is_valid_tree.py
@@ -0,0 +1,8 @@
+"""
+ graph, n nodes,  0 to n-1;  a list of edges
+tell if the graph is valid tree
+e.g.
+case1: n =4  [[0,1], [1,2], [1,3]]  yes   
+case2: n =4  [[0,1], [1,2], [0, 2]] no  0 -> 1 ->2
+case3 : n =4 [[0,1],[2,3]] no  
+"""
